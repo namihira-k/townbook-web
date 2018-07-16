3b4b9e1767ffddaa83dd22afc88b73728b150f3a
namihira-k <kosuke.namihira@gmail.com>
2018-07-16 11:22:21 +0900

    initial commit

---
+++ src/main/java/jp/co/namihira/townbookweb/TownbookWebApplication.java
@@ -0,0 +1,12 @@
+package jp.co.namihira.townbookweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TownbookWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TownbookWebApplication.class, args);
+	}
+}
