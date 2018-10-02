b8baf56a22ed6ad324e47037631228bf4425eb8c
namihira-k <kosuke.namihira@gmail.com>
2018-10-02 23:01:34 +0900

    update security setup

--- src/main/java/jp/co/namihira/townbookweb/config/AppSecurityConfig.java
+++ src/main/java/jp/co/namihira/townbookweb/config/AppSecurityConfig.java
@@ -1,15 +1,11 @@
 package jp.co.namihira.townbookweb.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
 
 @Configuration
 @EnableWebSecurity
@@ -19,27 +15,21 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/", "/api/**", "/view/**", "/js/**", "/error").permitAll()
-                .anyRequest().authenticated()
+//                .antMatchers("/view/eventadd").hasRole("USER")
                 .and()
             .formLogin()
                 .loginPage("/view/login")
-                .permitAll()
+                .failureUrl("/view/login?error")
                 .and()
             .logout()
                 .permitAll();
     }
 
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-             User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+				.withUser("user").password("password").roles("USER");
+	}    
 
-        return new InMemoryUserDetailsManager(user);
-    }
-	
 }
