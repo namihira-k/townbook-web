164f146c0012c2f63a92d48329b9cf50a8bdf954
namihira-k <kosuke.namihira@gmail.com>
2018-11-04 23:04:16 +0900

    add twitter dm

--- src/main/java/jp/co/namihira/townbookweb/controller/api/eventrequest/EventRequestApiController.java
+++ src/main/java/jp/co/namihira/townbookweb/controller/api/eventrequest/EventRequestApiController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jp.co.namihira.townbookweb.controller.api.AbstractApiController;
 import jp.co.namihira.townbookweb.dto.EventRequestDto;
 import jp.co.namihira.townbookweb.service.eventrequest.EventRequestService;
+import jp.co.namihira.townbookweb.service.twitter.TwitterService;
 
 @RestController
 public class EventRequestApiController extends AbstractApiController {
@@ -19,9 +20,13 @@ public class EventRequestApiController extends AbstractApiController {
 	@Autowired
 	private EventRequestService eventRequestService;
 	
+	@Autowired
+	private TwitterService twitterService;
+	
 	@PostMapping(BASE_PATH)
 	@ResponseStatus(HttpStatus.CREATED)
 	public EventRequestDto post(@RequestBody EventRequestDto dto) {
+		twitterService.postDM("yorimichi_jp", "posted event request! : " + dto.getName() + ", " + dto.getUrl());
 		return eventRequestService.save(dto);
     }
 	
