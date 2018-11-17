86f05b0c334d5c5c2237925c10ba5d97581083ea
namihira-k <kosuke.namihira@gmail.com>
2018-11-17 21:51:38 +0900

    fix towerrecord data

--- src/main/java/jp/co/namihira/townbookweb/controller/api/dev/EventDevApiController.java
+++ src/main/java/jp/co/namihira/townbookweb/controller/api/dev/EventDevApiController.java
@@ -3,6 +3,8 @@ package jp.co.namihira.townbookweb.controller.api.dev;
 import java.io.IOException;
 import java.text.MessageFormat;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -22,37 +24,53 @@ public class EventDevApiController extends AbstractApiController {
 		
 	@GetMapping("/dev/eventfetch")
 	public String get() throws IOException {
+		String result = "";
 		
-		String result = "(";
-		
-		final MessageFormat mf = new MessageFormat("''{0}''");
-				
 		Document doc = towerRecordClient.getEvent();
-		
 		Elements elements = doc.getElementsByClass("textbox");
-		Element element = elements.get(0);
 		
-		Element inner = element.getElementsByClass("inner").get(0);		
-		Element title = inner.getElementsByTag("h6").get(0).getElementsByTag("a").get(0);
+		int id = 100;
 		
-		result += toValueOnSQL(title.text());
-		
-		String place = "タワーレコード渋谷店";
-		result += toValueOnSQL(place);
-		
-		result += toValueOnSQL("13");
-		result += toValueOnSQL("22715");
-		
-		result += toValueOnSQL(title.attr("name"));
-		
-		result += toValueOnSQL("end");
+		for (int i = 0; i < elements.size(); i++, id++) {
+			String str = String.valueOf(id) + ",";
 
-		result += toValueOnSQL("condition");
-		result += toValueOnSQL("");
+			Element element = elements.get(i);
+			Element inner = element.getElementsByClass("inner").get(0);		
+			Element title = inner.getElementsByTag("h6").get(0).getElementsByTag("a").get(0);
+			
+			str += toValueOnSQL(title.text());
+			
+			String place = "タワーレコード渋谷店";
+			str += toValueOnSQL(place);
+			
+			str += toValueOnSQL("13");
+			str += toValueOnSQL("22715");
+			
+			final String date = title.attr("name");
+			
+			String content = inner.getElementsByClass("content_title").get(0).text();
+			Pattern p = Pattern.compile("([0-9]+).+?([0-9]+)");
+			Matcher m = p.matcher(content);
+			if (m.find()) {
+				str += toValueOnSQL(date + " " + m.group(1) + ":" + m.group(2) + ":00");
+				str += toValueOnSQL(date + " " + (Integer.parseInt(m.group(1))+1) + ":" + m.group(2) + ":00");
+			}
+			
+			// condition
+			str += toValueOnSQL("");
+			
+			String url = title.attr("href");
+			str += toValueOnSQL(url);
+
+			// content
+			str += toValueOnSQL("");			
+			
+			str += toValueOnSQL(UUID.randomUUID().toString());
+			
+			result += "(" + str.substring(0, str.length()-2) + "),";			
+		}
 		
-		result += toValueOnSQL(UUID.randomUUID().toString());
-		
-		return result + ")";
+		return result.substring(0, result.length()-1) + ";";
     }
 	
 	private String toValueOnSQL(final String str) {
