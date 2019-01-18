c52469b8c6f86b3504778a254c06906dc380b5d6
namihira-k <kosuke.namihira@gmail.com>
2019-01-18 21:25:50 +0900

    add clock

---
+++ src/main/resources/static/js/common/script.js
@@ -0,0 +1,26 @@
+new Vue({
+  el: '#header',
+  data () {
+		return {
+			datetime: '---------- --:--',
+		};
+  },
+  
+  mounted () {
+  	let self = this;
+    setInterval(function () {self.updateTime()}, 60000);  	
+  },
+
+  methods: {
+  	updateTime () {
+  		this.datetime = this.formatDate(new Date());
+  	},
+  	
+  	formatDate (date) {
+  		return date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate() + " " 
+  		     + date.getHours() + ":" + date.getMinutes();
+  	},
+  	
+  },
+  
+})
