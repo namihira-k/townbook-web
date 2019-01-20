69e1b65a2f4ff7a511391416e3f5aba77f742f89
namihira-k <kosuke.namihira@gmail.com>
2019-01-20 09:00:27 +0900

    add notify init data

--- src/main/java/jp/co/namihira/townbookweb/service/twitter/TwitterService.java
+++ src/main/java/jp/co/namihira/townbookweb/service/twitter/TwitterService.java
@@ -1,6 +1,7 @@
 package jp.co.namihira.townbookweb.service.twitter;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import jp.co.namihira.townbookweb.client.twitter.TwitterClient;
@@ -11,8 +12,15 @@ public class TwitterService {
 	@Autowired
 	private TwitterClient twitterClient;
 	
+	@Value("${twitter.account}")
+	private String twitterAccount = "";
+	
 	public void postDM(final String screenName, final String text) {
 		twitterClient.postDM(screenName, text);
+	}
+	
+	public void postDMtoAdmin(final String text) {
+		twitterClient.postDM(twitterAccount, text);
 	}
 	
 }
