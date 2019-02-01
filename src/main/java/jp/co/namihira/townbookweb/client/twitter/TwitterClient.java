bbdcf374fcd5fe9865993815afbb2f3360bd8efe
namihira-k <kosuke.namihira@gmail.com>
2019-02-01 20:56:32 +0900

    add twitter cron

--- src/main/java/jp/co/namihira/townbookweb/client/twitter/TwitterClient.java
+++ src/main/java/jp/co/namihira/townbookweb/client/twitter/TwitterClient.java
@@ -3,6 +3,7 @@ package jp.co.namihira.townbookweb.client.twitter;
 import org.springframework.stereotype.Service;
 
 import twitter4j.DirectMessage;
+import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -10,16 +11,26 @@ import twitter4j.TwitterFactory;
 @Service
 public class TwitterClient {
 
-	private static final Twitter sender = TwitterFactory.getSingleton();
-	
-	public DirectMessage postDM(final String screenName, final String text) {
+    private static final Twitter sender = TwitterFactory.getSingleton();
+
+    public DirectMessage postDM(final String screenName, final String text) {
         DirectMessage dm = null;
         try {
-			dm = sender.sendDirectMessage(screenName, text);
-		} catch (TwitterException e) {
-			e.printStackTrace();
-		}
-		return dm;
+            dm = sender.sendDirectMessage(screenName, text);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        return dm;
+    }
+
+    public Status tweet(final String text) {
+        Status status = null;
+        try {
+            status = sender.updateStatus(text);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        return status;
     }
 
 }
